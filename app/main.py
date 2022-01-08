54b670a34af964d7b9375d21e6f3eb2fec8cf92b
mariaramperez <79089888+mariaramperez@users.noreply.github.com>
2022-01-08 17:07:07 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -36,5 +36,5 @@ def bye_world():
 	return ("Adios mundo cruel")
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=PORT)
+    app.run(debug=True, host='0.0.0.0', PORT=PORT)
 
